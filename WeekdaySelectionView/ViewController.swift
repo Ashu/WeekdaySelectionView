6ad980444d3ba77f6f4862b824abeb70823394ee
Ashu <Ashu@users.noreply.github.com>
2020-08-11 23:50:25 +0530

    Initial Commit

---
+++ WeekdaySelectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekdaySelectionView
+//
+//  Created by Ashutosh Dave on 11/08/20.
+//  Copyright © 2020 Ashutosh Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
